d37b08e3e360a644ab6d08dc9843e4225ac038ab
poli <xyz@gmail.com>
2024-02-14 09:26:53 +0530

    My first commit

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Integer
+        byte b = 127;
+        System.out.println("Byte usage: " + (++b));
+        short s = 32767;
+        System.out.println("Short usage: " + (++s));
+        int i = 32767;
+        System.out.println("Int usage: " + (++i));
+        long l = 32676L;
+        System.out.println("Long usage: " + (++l));
+
+
+        //floating point data type
+        float f = 1.1f;
+        System.out.println("Float usage: " + f);
+
+        final double d = 3.14;
+        System.out.println("Double usage: " +d);
+
+        // Character data type
+        char c = 1001;
+        System.out.println("Char usage: " +c);
+
+        // Boolean data type
+        boolean bool = true;
+        System.out.println("Boolean usage:" +bool);
+
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    public static void main(String[] args) {
+         int num1 = 100, num2 = 20;
+         System.out.println( num2 + num1);
+    }
+}
